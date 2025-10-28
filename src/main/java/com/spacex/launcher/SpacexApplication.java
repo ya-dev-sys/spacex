976f71092e379b0dca07d17bfbf6c4498e302015
yanis <finagnonallade@gmail.com>
2025-10-28 23:36:50 +0100

    Fix CI

--- src/main/java/com/spacex/launcher/SpacexApplication.java
+++ src/main/java/com/spacex/launcher/SpacexApplication.java
@@ -4,14 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
-@EntityScan("com.spacex.launcher.model")
-@EnableJpaRepositories("com.spacex.launcher.repository")
 public class SpacexApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(SpacexApplication.class);
@@ -19,7 +15,5 @@ public class SpacexApplication {
     public static void main(String[] args) {
         logger.info("Starting spacex-backend application");
         SpringApplication.run(SpacexApplication.class, args);
-
     }
-
 }
